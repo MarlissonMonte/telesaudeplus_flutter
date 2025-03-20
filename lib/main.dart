799d35e4eb979728ad60dccbacb0863023f1e259
joaopaulo0007 <joaomendonca865@gmail.com>
2025-03-20 15:50:41 -0300

    Configuring API URL and updating doctor model to be nullable and more robust.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'routes/app_routes.dart';
+import 'services/notification_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Inicializa o serviço de notificações
+  final notificationService = NotificationService.instance;
+  await notificationService.initialize();
+
   runApp(const MyApp());
 }
 
